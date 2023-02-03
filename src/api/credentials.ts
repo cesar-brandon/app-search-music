30a77f0269fa3fb0497643edb5aa098a5607b5b1
Cesar Brandon <cesarbrandon000@gmail.com>
2023-02-03 02:19:22 -0500

    🤡

--- src/api/credentials.ts
+++ src/api/credentials.ts
@@ -1,8 +1,3 @@
-// development
-// export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-// export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
-// export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
-// production
-export const CLIENT_ID = process.env.VITE_CLIENT_ID;
-export const CLIENT_SECRET = process.env.VITE_CLIENT_SECRET;
-export const REDIRECT_URI = process.env.VITE_REDIRECT_URI;
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
